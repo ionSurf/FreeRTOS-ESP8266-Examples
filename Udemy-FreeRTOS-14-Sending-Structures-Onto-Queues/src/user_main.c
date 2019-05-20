407339c13f6c1aa55ed045e5090561b9294280fd
ionSurf <1911873+ionSurf@users.noreply.github.com>
2019-05-20 17:58:32 -0400

    Update user_main.c

--- Udemy-FreeRTOS-14-Sending-Structures-Onto-Queues/src/user_main.c
+++ Udemy-FreeRTOS-14-Sending-Structures-Onto-Queues/src/user_main.c
@@ -1,36 +1,91 @@
-//  Add libraries
-//  Add profilers
+// setup() function
+// Create queue w/handler
+// Create 2 tasks referencing one same function for sending
+// Create 1 task to receive, wait 100 ticks to receive
 
-/*  Use these structures
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "esp_common.h"
+#include <stdio.h>
 
-typedef enum
-{
-  eSender1,
-  eSender2,
-}DataSource_t;
+uint32 user_rf_cal_sector_set(void) {
+    flash_size_map size_map = system_get_flash_size_map();
+    uint32 rf_cal_sec = 0;
+    switch (size_map) {
+        case FLASH_SIZE_4M_MAP_256_256:
+            rf_cal_sec = 128 - 5;
+            break;
 
-typedef struct
-{
-  uint8_t ucValue;
-  DataSource_t eDataSource;
-}Data_t;
+        case FLASH_SIZE_8M_MAP_512_512:
+            rf_cal_sec = 256 - 5;
+            break;
 
+        case FLASH_SIZE_16M_MAP_512_512:
+        case FLASH_SIZE_16M_MAP_1024_1024:
+            rf_cal_sec = 512 - 5;
+            break;
 
-static const Data_t xStructToSend[2]=
-{
-  {100,eSender1},
-  {50,eSender2}
-};
-*/
+        case FLASH_SIZE_32M_MAP_512_512:
+        case FLASH_SIZE_32M_MAP_1024_1024:
+            rf_cal_sec = 1024 - 5;
+            break;
 
-// Create queue handle
+        default:
+            rf_cal_sec = 0;
+            break;
+    }
 
-// Setup function:
+    return rf_cal_sec;
+}
 
-// Create queue
+#define TicksTOSEND    0
+#define TicksTORECEIVE 100
 
-// Create 2 tasks sending each element in the array
+#define DELAYTOSEND    1
+#define DELAYTORECEIVE 1
 
-// Create task receiving data
+xQueueHandle myQueue;
 
-// Receiving task must print who wrote to the queue.
+void setup () {
+    myQueue = xQueueCreate( 5, sizeof( int32_t ) );
+
+    xTaskCreate( &TxTask, "TxTask1", 100, NULL, 1, NULL );
+    xTaskCreate( &TxTask, "TxTask2", 100, NULL, 1, NULL );
+    xTaskCreate( &RxTask, "RxTask", 100, NULL, 1, NULL );
+}
+
+void TxTask ( void* pvParams ) {
+    portBASE _TYPE status;
+    uint8_t message = 1990;
+
+    while( 1 ) {
+        status = xQueueSend( myQueue, &message, TicksTOSEND );
+        if ( status != pdPASS )
+            printf( "Could not write to queue" );
+        
+        printf( "Sent %d to queue from ", message );
+        vTaskDelay( DELAYTOSEND );
+    }
+}
+
+void RxTask ( void* pvParams ) {
+    portBASE _TYPE status;
+    uint8_t message;
+
+    while( 1 ) {
+        status = xQueueReceive( myQueue, &message, TicksTORECEIVE );        
+        if ( status != pdPASS )
+            printf( "Could not read from queue" );
+        
+        printf( "Received %d from queue from ", message );
+        vTaskDelay( DELAYTORECEIVE );
+    }
+
+}
+
+void user_init ( void ) {
+    printf("\nUdemy FreeRTOS Course - 13 Writing to Queue from 2 tasks\n");
+
+    setup();
+}
